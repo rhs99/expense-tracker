e45a41fb5810565215e6ffa2e80f3965e1911020
Rakibul Hasan <rhrana99@gmail.com>
2022-08-19 12:48:05 +0600

    add state

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,12 @@
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card"
+import Card from "./Card";
 import "./ExpenseItem.css";
 
-
 function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("Clicked!!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
@@ -11,6 +14,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
